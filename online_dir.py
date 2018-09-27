eff19845dec026fb3d83284041623ee070c0df43
Marianne Fletcher <mfletche@astral.cms.waikato.ac.nz>
2018-09-28 10:20:03 +1200

    Create cl class to handle online directory listing.

---
+++ online_dir.py
@@ -0,0 +1,47 @@
+"""Represents an online directory on the routeview website. Can fetch the HTML
+representing the directory and list files and folders.
+"""
+
+import pycurl
+from StringIO import StringIO
+
+baseUrl = 'http://archive.routeviews.org/route-views6/bgpdata/'
+
+class OnlineDir(Base):
+    
+    def __init__(self, url):
+        if url:
+            self.url = url
+        else:
+            # Use default
+            self.url = baseUrl
+            
+    def fetch(self):
+        """ Fetch the HTML describing the online directory.
+        """
+        # Use Curl to fetch resource
+        buffer = StringIO()
+        c = pycurl.Curl()
+        c.setopt(c.URL, url)
+        c.setopt(c.WRITEDATA, buffer)
+        c.perform()
+    
+        # Check HTTP response code indicates OK
+        if (c.getinfo(c.RESPONSE_CODE) != 200):
+            print(str(c.RESPONSE_CODE) + ' error: Could not fetch ' + url)
+            return
+    
+        c.close()
+    
+        self.body = buffer.getvalue()
+        buffer.close()
+        return body
+    
+def main():
+    dir = OnlineDir()
+    dir.fetch()
+    print(dir.body)
+
+if __name__ == "__main__":
+    main()
+
