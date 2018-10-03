d213c1321387564cbdd6c9703f07411cb3423578
Marianne Fletcher <mfletche@astral.cms.waikato.ac.nz>
2018-10-03 13:07:57 +1300

    Redo main function to use new classes.

--- rv_ingest.py
+++ rv_ingest.py
@@ -1,36 +1,10 @@
 from rv_catalogue import RVCatalogue
-import arrow
-import pycurl
-from rib_file import BgpDump
-from mrtparse import *
+from cass_interface import CassInterface
 import mrt_file
-import subprocess
-from cassandra.cluster import Cluster
-from cassandra import ReadTimeout
+from _ast import Pass
 
-logoutput = sys.stdout
-
-tmpname = 'tmp.csv'
-r8_ip = '130.217.250.114'
-
-# Schema for cassandra-loader
-bgpevents_schema = '"bgp6.bgpevents(prefix,ts,sequence,peer,peerip,type,aspath)"'
-rib_schema = '"bgp6.rib(prefix,peer,peerip,snapshot,ts,aspath)"'
-
-# Copy statements for alternative method of bulk loading
-events_copy = "COPY bgp6.bgpevents (prefix, ts, sequence, peer, peerip, type, aspath) FROM "
-rib_copy = "COPY bgp6.rib (prefix, peer, peerip, snapshot, ts, aspath) FROM "
-copy_options = " WITH DELIMITER = '|'"
-
-# This class is just so I can construct an args object for Bgpdump manually
-class Object(object):
-    pass
-
-# BGPDump arguments
-bgpargs = Object()
-bgpargs.verbose = False
-bgpargs.ts_format = 'dump'
-bgpargs.pkt_num = False
+RIB_META_NAME = 'importedrib'
+UPDATES_META_NAME = 'imported'
 
 def fetch_file(url, tofile):
     with open(tofile, 'w') as local:
@@ -41,31 +15,12 @@ def fetch_file(url, tofile):
         c.close()
         local.close()
 
-def convert_mrt_to_csv(input, output, forceRIB=False):
-    # Convert to CSV file
-    d = Reader(input)
-    bgpargs.output = open(tmpname, 'w')
-    count = 0
-    for m in d:
-        m = m.mrt
-        if m.err:
-            continue
-        b = BgpDump(bgpargs, forceRIB)
-        if m.type == MRT_T['TABLE_DUMP']:
-            b.td(m, count)
-        elif m.type == MRT_T['TABLE_DUMP_V2']:
-            b.td_v2(m)
-        elif m.type == MRT_T['BGP4MP']:
-            b.bgp4mp(m, count)
-        count += 1
-
-cluster = Cluster([r8_ip])
-session = cluster.connect('bgp6')
+db = CassInterface()
+logoutput = sys.stdout
 
 for remotefile in RVCatalogue().listDataAfter(
     'http://archive.routeviews.org/route-views6/bgpdata/',
     arrow.get(2018, 9, 25, 0, 0)):
-
     
     # Work out filename
     localfile = remotefile.rsplit('/', 1)[-1]
@@ -78,39 +33,26 @@ for remotefile in RVCatalogue().listDataAfter(
             continue 
         
     if localfile.startswith('rib'):
-        datafile = mrt_file.ProcessedRIBFile(localfile, tmpname, session)
-        insert_q = rib_copy
+        type = 'RIB'
     elif localfile.startswith('updates'):
-        datafile = mrt_file.ProcessedUpdatesFile(localfile, tmpname, session)
-        insert_q = events_copy
+        type = 'Updates'
     else:
         sys.stderr.write('Cannot determine format: %s' % (localfile))
-        exit()
-    
-    if datafile.is_file_inserted():
-        logoutput.write('Already inserted file: %s\n' % (remotefile))
         continue
     
-    logoutput.write('Fetching remote file: %s\n' % (remotefile))
-    fetch_file(remotefile, localfile)
-    logoutput.write('Fetched remote file: %s\n' % (remotefile))
-    
-    logoutput.write('Converting to CSV\n')
-    if localfile.startswith('rib'):
-        convert_mrt_to_csv(localfile, tmpname, forceRIB=True)
-    else:
-        convert_mrt_to_csv(localfile, tmpname)
-    logoutput.write('Converted to CSV\n')
-    
-    logoutput.write('Beginning copy\n')
-    cmd = 'cqlsh ' + r8_ip + ' -e "' + insert_q + ("'%s'" % tmpname) + copy_options + '"'
-    print(cmd)
-    r = subprocess.call(cmd, shell=True)
-    logoutput.write('Copy finished\n')
-    datafile.set_file_inserted(True)
+    if not db.is_file_ingested(localfile, RIB_META_NAME if type == 'RIB' else UPDATES_META_NAME):
+        # Do the actual fetching of the file
+        logoutput.write('Fetching remote file: %s\n' % (remotefile))
+        fetch_file(remotefile, localfile)
+        logoutput.write('Fetched remote file: %s\n' % (remotefile))
+        
+        # Parse into lines and insert them into db
+        mrtfile = mrt_file.MRTExtractor(localfile)
+        for line in mrtfile.lines(type):
+            if type == 'RIB':
+                db.insert_rib(line)
+            else:
+                db.insert_updates(line)
 
-    #loader_args = ['-f', '%s' % (tmpname), '-host', '130.217.250.114', '-schema', '%s' % (rib_schema)]
-    
-    # An alternative option to bulk load into Cassandra
-    #r = subprocess.call(['/home/mfletche/opt/cassandra-loader', loader_args])
-    #print('Return code: %s' % (r))
+        logoutput.write('Completed ingesting file: %s\n' % localfile)
+        db.set_file_ingested(localfile, True, RIB_META_NAME if type == 'RIB' else UPDATES_META_NAME)
